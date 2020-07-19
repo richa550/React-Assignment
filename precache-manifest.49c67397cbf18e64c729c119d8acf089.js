88f47c34d68d16077e0dfcfe6e9393409afe5f3c
richa550 <richagupta550@gmail.com>
2020-07-19 18:47:30 +0530

    Updates

---
+++ precache-manifest.49c67397cbf18e64c729c119d8acf089.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e66d7e3450d4086a2f23a1b6d6d2ba",
+    "url": "/React-Assignment/index.html"
+  },
+  {
+    "revision": "345cc699961816b3bcc3",
+    "url": "/React-Assignment/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "22d5650304d02b9661b9",
+    "url": "/React-Assignment/static/css/main.65c6a07d.chunk.css"
+  },
+  {
+    "revision": "345cc699961816b3bcc3",
+    "url": "/React-Assignment/static/js/2.1c731d2e.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/React-Assignment/static/js/2.1c731d2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22d5650304d02b9661b9",
+    "url": "/React-Assignment/static/js/main.89a16f55.chunk.js"
+  },
+  {
+    "revision": "100985f2260c7e399d07",
+    "url": "/React-Assignment/static/js/runtime-main.69602fc1.js"
+  }
+]);
